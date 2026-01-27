10fec1da07ee43552a127727b9e8ef3dbc367291
priyanka-n220739 <n220739@rgukt.ac.in>
2026-01-27 19:12:52 +0530

    added index.php and complltd fronted task4

---
+++ Iindx.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Newr - Weekly Tasks</title>
+</head>
+<body>
+    <h2>Weekly Tasks</h2>
+    <ul>
+        <li><a href="task_01/">Task 1</a></li>
+        <li><a href="task_02/">Task 2</a></li>
+        <li><a href="task_03/">Task 3</a></li>
+        <li><a href="task_04/">Task 4 (Final Frontend)</a></li>
+    </ul>
+</body>
+</html>
